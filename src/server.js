859421533357d875d8cae22b40d0e2d5443e9be6
michelbaretta <baretta.michel@gmail.com>
2023-01-16 01:40:06 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
-import path from "path";
-import productRouter from "./routers/ProductRouter.js";
-import userRouter from "./routers/UserRouter.js";
-import dotenv from "dotenv";
-import orderRouter from "./routers/OrderRouter.js";
-import uploadRouter from "./routers/UploadRouter.js";
-import cors from "cors";
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import productRouter from './routers/ProductRouter.js';
+import userRouter from './routers/UserRouter.js';
+import dotenv from 'dotenv';
+import orderRouter from './routers/OrderRouter.js';
+import uploadRouter from './routers/UploadRouter.js';
+import cors from 'cors';
 
 dotenv.config();
 
-console.log("a");
+console.log('a');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -20,21 +20,21 @@ app.use(express.urlencoded({ extended: true }));
 
 // ñ
 mongoose
-  .connect(process.env.MONGODB_URL || "mongodb://localhost:27017/ecommerce", {
+  .connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/ecommerce', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.log(error));
-app.use("/api/uploads", uploadRouter);
-app.use("/api/users", userRouter);
-app.use("/api/products", productRouter);
-app.use("/api/orders", orderRouter);
-app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+app.use('/api/uploads', uploadRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-app.get("/api/config/google", (req, res) => {
-  res.send(process.env.GOOGLE_API_KEY || "");
+app.get('/api/config/google', (req, res) => {
+  res.send(process.env.GOOGLE_API_KEY || '');
 });
 
 // mercadopago.configure({
@@ -71,10 +71,10 @@ app.get("/api/config/google", (req, res) => {
 // });
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/public/index.html'))
 );
 // app.get("/", (req, res) => {
 //   res.send("Server is ready.");
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const host = process.env.HOST || "0.0.0.0";
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 app.listen(port, host, () => {
   console.log(`Server at http://localhost:${port}`);
